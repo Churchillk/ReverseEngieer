6f86ba58095373876309c54204742525ad2fa80e
Churchillk <132873688+Churchillk@users.noreply.github.com>
2023-11-28 02:39:46 -0500

    update

--- reverse1.py
+++ reverse1.py
@@ -1,13 +1,26 @@
-import os
-os.system("pip install capstone")
 from capstone import Cs, CS_ARCH_X86, CS_MODE_32
 
-# Replace the shellcode with your actual machine code
-shellcode = b"\x8d\x4c\x32\x08\x01\xd8\x81\x78\x08\x64\x61\x74\x61\x75\xf4\x89\x74\x24\xfc"
+def disassemble_user_input(shellcode):
+    # Initialize Capstone disassembler
+    md = Cs(CS_ARCH_X86, CS_MODE_32)
 
-# Initialize Capstone disassembler
-md = Cs(CS_ARCH_X86, CS_MODE_32)
+    # Disassemble the user-provided machine code
+    for i in md.disasm(shellcode, 0x1000):
+        print(f"0x{i.address:x}:\t{i.mnemonic}\t{i.op_str}")
 
-# Disassemble the machine code
-for i in md.disasm(shellcode, 0x1000):
-    print(f"0x{i.address:x}:\t{i.mnemonic}\t{i.op_str}")
+def main():
+    # Receive a byte input from the user
+    user_input_hex = input("Enter the shellcode in hexadecimal format: ")
+
+    # Convert the hexadecimal string to bytes
+    try:
+        shellcode = bytes.fromhex(user_input_hex)
+    except ValueError:
+        print("Invalid input. Please enter a valid hexadecimal string.")
+        return
+
+    # Disassemble the user-provided machine code
+    disassemble_user_input(shellcode)
+
+if __name__ == "__main__":
+    main()
